05c5c3fd32dc342b076bd064693c02d6511881c4
mizchi <miz404@gmail.com>
2020-05-04 23:31:09 +0900

    Add sw and amp-script

---
+++ pages/try-script.tsx
@@ -0,0 +1,17 @@
+import { Layout } from "../components/Layout";
+import { AmpScript } from "../components/AmpScript";
+
+export default () => {
+  const host =
+    process.env.NODE_ENV === "production"
+      ? "https://mizchi.dev/"
+      : "http://localhost:3000/";
+
+  return (
+    <Layout>
+      <AmpScript layout="container" src={`${host}/static/amp-script/hello.js`}>
+        <button>Hello amp-script!</button>
+      </AmpScript>
+    </Layout>
+  );
+};
